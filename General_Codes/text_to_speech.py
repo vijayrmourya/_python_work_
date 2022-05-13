8c63026315fd3060cca5a3c529c602b4149bbf5a
bloodborne <vijayrmourya@gmail.com>
2022-05-13 20:11:40 +0530

    updated

--- General_Codes/text_to_speech.py
+++ General_Codes/text_to_speech.py
@@ -1,7 +1,9 @@
 import pyttsx3
 import PyPDF2
 
-pdf_file = "G:\__Entire_Github__\__Languages\Python\py_ignore_course_data\Data_structure_Udemy_competency\Data_Structure_&_Algorithm_The_Complete_Masterclass\Visualizing_Algorithm\Why+algorithm+execution+time+is+different+for+every+time_.pdf"
+pdf_file = "G:\__Entire_Github__\__Languages\Python\py_ignore_course_data\Data_structure_Udemy_competency" \
+           "\Data_Structure_&_Algorithm_The_Complete_Masterclass\Visualizing_Algorithm\Why+algorithm+execution+time" \
+           "+is+different+for+every+time_.pdf "
 
 obj = open(pdf_file, 'rb')
 pdfread = PyPDF2.PdfFileReader(obj)
